2e6718fc3eb6cfb08db5bff4dd1e5a4d2178c8d2
khurand <kevin.hurand@gmail.com>
2020-10-06 21:21:22 +0200

    !

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand href='/'>Armor Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
