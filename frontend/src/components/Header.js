06add48d2b9f602a496c49e2557fd031b72d8234
HURAND Kevin <kevin.hurand@gmail.com>
2020-10-06 21:25:28 +0200

    !!

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
-      <Navbar variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Armor Shop</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
+          <Navbar.Brand href="/">Armor Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
