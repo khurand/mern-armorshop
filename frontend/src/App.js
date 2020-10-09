fb56a1477cfc16f9dc70e38378cd979898ac9994
khurand <kevin.hurand@gmail.com>
2020-10-09 16:22:39 +0200

    homeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1>Bonjour</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
